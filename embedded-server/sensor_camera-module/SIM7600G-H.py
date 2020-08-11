63d5581d4d9b3543c22d6bdf902f82fa09cc9f4a
cbarange <cbarange.dev@gmail.com>
2020-08-11 17:49:06 +0200

    add skeleton project

---
+++ embedded-server/sensor_camera-module/SIM7600G-H.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "cbarange"
+__license__ = "MIT"
+__date__ = "11-08-2020"
+__version__ = "1.0.0"
+__status__ = "Prototype"
+
+import os    # standard library
+import sys
+
+def measure():
+	return 42
