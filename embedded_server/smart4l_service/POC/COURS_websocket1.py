c55a58619fe73f9a302358fc16cbcd0b90f5321b
cbarange <cbarange.dev@gmail.com>
2020-09-01 19:00:36 +0200

    Update Websocket tuto

--- embedded_server/smart4l_service/POC/COURS_websocket1.py
+++ embedded_server/smart4l_service/POC/COURS_websocket1.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 
-
-
+# Regarder un jeux multi websockets
+# Regarder Google doc websockets
+# regarder tornado websockets
 
 # --- Cours 1 ---
 """
@@ -31,19 +32,39 @@ asyncio.get_event_loop().run_forever()
 
 # --- -------------------------------------- ---
 
-# --- Cours 2 ---
+# --- Cours 2 ♥ ---
 """
 https://www.youtube.com/watch?v=8ARodQ4Wlf4
 """
-
-
+# Libraries :
+#flask_sockets
+#python_socketio
+#flask_socketio
 
 # --- --------------------------------------- ---
 
-# --- Cours 3 ---
+# --- Cours 3 ♥ ---
 """
 https://www.youtube.com/watch?v=6oQuQ5coSQ0
 """
+
+# libraries
+# tornado, async web framework
+# django is sync web framework, look django channels
+# websockets
+
+"""
+avec la classe WebSocketHandler
+1 - Faire une liste de tous les websockets
+2 - Faire une liste de tous les messages
+
+onOpen → add client to wsList, send all the messages to the new client
+onMessage → add it to messageList and send it to every client 
+onClose → delete client from wsList
+
+En websockets il faut implémenter le drop de connexion, les erreurs diverses etc...
+"""
+
 
 
 
@@ -62,9 +83,49 @@ https://www.youtube.com/watch?v=AcZJUQ3y2ek
 """
 https://www.youtube.com/watch?v=F3pB0PHcUxE
 """
+# Code a revoir
+
+# ws_server
+import ssl
+import pathlib
+import asyncio
+import websockets
+
+async def response(websocket, path):
+	message = await websocket.recv()
+	print(f"Message from client : {message}")
+	await websocket.send("Message recv confirmation")
+
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+ssl_context.load_cert_chain(pathlib.Path(__file__).with_name("mycert.pem"), keyfile=pathlib.Path(__file__).with_name("mykey.key"))
+
+
+start_server = websockets.serve(response, 'localhost', 8520, ssl = ssl_context)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
+# ws_client TODO Find WSS with websockets not websocket
+import asyncio
+import websocket
+
+async def message():
+	ws = websocket.WebSocket(sslopt={"cert_reqs":ssl.CERT_NONE})
+	ws.connect("wss://localhost:8520")
+	msg = input("Message to send >")
+	ws.send(msg)
+	print(ws.recv())
+
+
+asyncio.get_event_loop().run_until_complete(message())
 
 
 
+# --- Cours 6 ---
+"""
+https://www.youtube.com/watch?v=L5YQbNrFfyw
+"""
 
 
 
+# --- --------------------------------------- ---
