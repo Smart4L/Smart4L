7aadc4ffa5a0e3f6d4ebdd88800f2c624061cb38
Jules Peguet <julespeguet.jp@gmail.com>
2022-03-26 15:38:37 +0100

    reset web-client

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
-import './assets/sass/main.scss'
-import { Main } from './components/main/Main';
-import { Navbar } from './components/navbar/Navbar';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Main/>
-      <Navbar/>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- web-client/src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- * @param {String} str String you want to Capitalize the first letter
- * @return {String} Your string Capitalized
- */
-export const Capitalize = (str) => {
-    return str.charAt(0).toUpperCase() + str.slice(1)
-};
